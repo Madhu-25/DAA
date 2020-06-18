28fcd15b0fdbf82134d29cd429a4fcc9f3f58f26
Madhu-25 <65850954+Madhu-25@users.noreply.github.com>
2020-06-18 18:24:43 +0530

    Create maxProfit.py

---
+++ Greedy-Alg/maxProfit.py
@@ -0,0 +1,28 @@
+'''
+List A contains the profit per click on each ad
+List B contains the average number of clicks on each partition
+the function maxProfit returns the maximum profit we can get by arranging the ads in the partions
+sample input:
+3                                                                                                                                              
+1 3 -5                                                                                                                                         
+-2 4 1 
+Sample output:
+23
+'''
+
+
+def maxProfit(A,B):
+    S=0
+    while(A and B):
+        a=max(A)
+        b=max(B)
+        A.remove(a)
+        B.remove(b)
+        S+=(a*b)
+    return S
+
+
+n=int(input())
+A=[int(i) for i in input().split()]
+B=[int(i) for i in input().split()]
+print(maxProfit(A,B))
